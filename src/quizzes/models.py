2d995ff9090e48f068deada46e792e7628dd3b01
BKitor <bkitor@gmail.com>
2019-10-06 13:50:18 -0400

    starting game models
    cleaning up

--- src/quizzes/models.py
+++ src/quizzes/models.py
@@ -1,13 +1,15 @@
 import uuid
 from django.db import models
 
+from users.models import User
+
 # Create your models here.
 
 
 class Quiz(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
-    author = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __repr__(self):
         return self.name
--- src/src/settings.py
+++ src/src/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'quizzes',
+    'games',
     'rest_framework',
 ]
 
