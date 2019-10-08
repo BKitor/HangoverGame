514d80dc98404e37b374337026d75c4cc9621363
Sam McPhail <sam.mcphail9@gmail.com>
2019-10-08 14:33:55 -0400

    Create Question model

--- src/quizzes/models.py
+++ src/quizzes/models.py
@@ -8,3 +8,10 @@ class Quiz(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     author = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey("Quiz", on_delete=models.CASCADE)
+    #question type?
+    #text?
+    #
