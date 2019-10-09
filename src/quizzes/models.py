4dc047f2d4d39ac50bda276afc32aed675911eb2
Sam McPhail <sam.mcphail9@gmail.com>
2019-10-09 15:58:52 -0400

    Add skeletons for Quiz request methods

--- src/quizzes/models.py
+++ src/quizzes/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 
 
+# get related questions with <Quiz>.question_set.all()
 class Quiz(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
@@ -11,7 +12,13 @@ class Quiz(models.Model):
 
 
 class Question(models.Model):
+    default_text = "Enter question here"
+
     quiz = models.ForeignKey("Quiz", on_delete=models.CASCADE)
-    #question type?
-    #text?
-    #
+    text = models.CharField(max_length=100, default=default_text)
+    # question type?
+    # how are answers being done? are we still doing mulitple choice for some?
+    # how are we linking user's answers to a question/quiz?
+
+
+# Could create an answer class that has a connection to an author and to a question
--- src/quizzes/views.py
+++ src/quizzes/views.py
@@ -1,6 +1,8 @@
-from quizzes.models import Quiz
-from quizzes.serializers import QuizSerializer
+from quizzes.models import Quiz, Question
+from quizzes.serializers import QuizSerializer, QuestionSerializer
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -8,3 +10,26 @@ from rest_framework import generics
 class QuizListCreate(generics.ListCreateAPIView):
     queryset = Quiz.objects.all()
     serializer_class = QuizSerializer
+
+    def post(self, request):
+        print(request.data)
+        quiz = request.data
+        serializer = QuizSerializer(quiz)
+        print(serializer)
+        return Response(quiz)
+
+    def get(self, request):
+        quiz = Quiz.objects.all()
+        serializer = QuizSerializer(quiz, many=True)
+        return Response(serializer.data)
+
+    def put(self, request):
+        return Response(None)
+
+    def delete(self, request):
+        pass
+
+
+class QuestionListCreate(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
