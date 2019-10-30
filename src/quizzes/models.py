d4e11d4c4136570d4e6cdd2c0e7d7ade3a783b49
Sam McPhail <sam.mcphail9@gmail.com>
2019-10-29 22:06:18 -0400

    Added methods to easily access related quizzes from User and Question

--- src/quizzes/models.py
+++ src/quizzes/models.py
@@ -5,11 +5,13 @@ from django.conf import settings
 
 # Create your models here.
 
-# to get all associated quizzes - <question>.quiz_set.all()
 class Question(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     prompt = models.CharField(max_length=100)
     # question type?
+
+    def getQuizzes(self):
+        return self.quiz_set.all()
 
 
 class Quiz(models.Model):
