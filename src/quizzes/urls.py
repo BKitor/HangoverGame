fcac844902a21b671d81a4ba3abb22e8d5e6d29e
Sam McPhail <sam.mcphail9@gmail.com>
2019-11-26 10:12:45 -0500

    Axios doesn't allow get requests to send bodies, so need to be able to retrieve quiz by url

--- src/quizzes/urls.py
+++ src/quizzes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('api/quizzes', views.QuizListCreate.as_view()),
     path('api/questions', views.QuestionListCreate.as_view()),
+    path('quizzes/<pk>', views.QuizDetailView.as_view()),
 ]
